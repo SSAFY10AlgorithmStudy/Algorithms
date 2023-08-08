31c32d350984297653cc0bb4290d83b86d6f4663
DongJae <dkekzhs5@naver.com>
2023-08-08 23:13:10 +0900

    [feat] : "추가 노드 사이의 거리 1240 bfs, dfs"

--- src/S_1240.java
+++ src/S_1240.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.io.*;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -33,26 +34,49 @@ public class S_1240 {
             s = br.readLine().split(" ");
             int a = Integer.parseInt(s[0]);
             int b = Integer.parseInt(s[1]);
-            dfs(a, b, 0);
-            sb.append(answer).append("\n");
+            bfs(a, b);
+//            sb.append(answer).append("\n");
             Arrays.fill(visited, false);
             answer= 0 ;
         }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+//        bw.write(sb.toString());
+//        bw.flush();
+//        bw.close();
     }
 
-    private static void dfs(int a, int b, int d) {
-        if (a == b) {
-            answer = d;
-            return;
-        }
-        visited[a] = true;
-        for (Point p : list[a]) {
-            if (!visited[p.x]) {
-                dfs(p.x,b,d+p.y);
+    private static void bfs(int a, int b) {
+        ArrayDeque<Point> dq  = new ArrayDeque<Point>();
+        dq.add(new Point(a,0));
+        int ans[] = new int[n + 1];
+        while (!dq.isEmpty()) {
+            Point p = dq.poll();
+            visited[p.x] = true;
+
+            for (Point temP: list[p.x]) {
+                if (!visited[temP.x]) {
+                    ans[temP.x] = temP.y+ans[p.x];
+                    if (temP.x == b) {
+                        System.out.println(ans[b]);
+                        return ;
+                    }
+                    dq.add(temP);
+                }
+
             }
         }
     }
+
+
+//    private static void dfs(int a, int b, int d) {
+//        if (a == b) {
+//            answer = d;
+//            return;
+//        }
+//        visited[a] = true;
+//        for (Point p : list[a]) {
+//            if (!visited[p.x]) {
+//                dfs(p.x,b,d+p.y);
+//            }
+//        }
+//    }
 }
