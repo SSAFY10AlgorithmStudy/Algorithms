f07f4fec2b0da3aadc4252ebdb1816e72485d718
Sina <kseenyoung@gmail.com>
2023-09-10 00:05:56 +0900

    FEAT : 20230910 스터디

--- src/KSY/personal/Prectice/S1676_팩토리얼0의개수.java
+++ src/KSY/personal/Prectice/S1676_팩토리얼0의개수.java
@@ -3,6 +3,8 @@ package KSY.personal.Prectice;
 import java.util.*;
 import java.io.*;
 
+//11568KB/	76ms
+
 public class S1676_팩토리얼0의개수 {
 	
 	public static void main(String[] args) throws IOException {
@@ -11,26 +13,17 @@ public class S1676_팩토리얼0의개수 {
 		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());
-		if(N % 10 != 0 || N % 5 != 0) sb.append(0);
+		if(N == 0) sb.append(0);
 		else {
-			long sum = 1, answer=0;
-			//팩토리얼
-			for(int i=2; i<=N; i++) {
-				sum *= i;
-				if(sum > 1000)
-					sum %= 1000;
-			}
-			//System.out.println(sum);
-			//0이아닌 수 만날때까지 
-			while(true) {
-				if(sum % 10 == 0)
-					answer++;
-				else
-					break;
-				sum /= 10;
+			int answer=0;
+			while(N >= 5) {
+				answer += N / 5;
+				N /= 5;
 			}
 			sb.append(answer);
 		}
+		
+		
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
