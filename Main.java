53d10d8e4b20da35a504973ef877fb195400b486
박종우 <jongwoop97@gmail.com>
2023-10-24 22:23:30 +0900

    0923, 0930

--- Main.java
+++ Main.java
@@ -2,86 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static int n;
-    static int[][] map;
-    static List<int[]> students = new ArrayList<>();
-    static List<int[]> teachers = new ArrayList<>();
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 빠른 읽기
-        StringTokenizer st = null;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        for (int i=0;i<n; i++) {
+        HashMap<Integer, Integer> ladder = new HashMap<>();
+        HashMap<Integer, Integer> snake = new HashMap<>();
+
+        for (int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<n; j++) {
-                String s = st.nextToken();
-                switch (s) {
-                    case "S":
-                        map[i][j] = 1;
-                        students.add(new int[] {i,j});
-                        break;
-                    case "T":
-                        map[i][j] = 2;
-                        teachers.add(new int[] {i,j});
-                        break;
+            ladder.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        for (int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            snake.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+
+        int[] dp = new int[101];
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(1);
+
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            for (int i=1; i<=6; i++) {
+                int next = cur + i;
+                if (next == 100) {
+                    System.out.println(dp[cur] + 1);
+                    return;
+                }
+                if (ladder.containsKey(next)) {
+                    next = ladder.get(next);
+                } else if (snake.containsKey(next)) {
+                    next = snake.get(next);
+                }
+                if (dp[next] == 0) { // 안가본데 확인
+                    dp[next] = dp[cur] + 1;
+                    q.offer(next);
                 }
             }
         }
-
-        List<int[]> startPoint = null;
-        int find = -1;
-        if (students.size() <= teachers.size()) {
-            startPoint = students;
-            find = 2;
-        } else {
-            startPoint = teachers;
-            find = 1;
-        }
-
-        // 3중 루프해서 빈칸의 모든 조합에 벽을 세워서 확인
-        for (int i=0; i<n*n-2; i++) {
-            if (map[i/n][i%n] == 0) map[i/n][i%n] = -1;
-            for (int j=i+1; j<n*n-1; j++) {
-                if (map[j/n][j%n] == 0) map[j/n][j%n] = -1;
-                for(int k=j+1; k<n*n; k++) {
-                    if (map[k/n][k%n] == 0) map[k/n][k%n] = -1;
-                    if (simulate(startPoint, find)) {
-                        System.out.println("YES");
-                        return;
-                    }
-                    if (map[k/n][k%n] == -1) map[k/n][k%n] = 0;
-                }
-                if (map[j/n][j%n] == -1) map[j/n][j%n] = 0;
-            }
-            if (map[i/n][i%n] == -1) map[i/n][i%n] = 0;
-        }
-
-        System.out.println("NO");
-    }
-
-    static boolean simulate(List<int[]> startPoint, int find) {
-        int[] dr = {1, -1, 0, 0};
-        int[] dc = {0, 0, 1, -1};
-
-        for (int[] p: startPoint) {
-            int r = p[0];
-            int c = p[1];
-            for (int i=0; i<4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                while (nr >= 0 && nc >= 0 && nr < n && nc < n && map[nr][nc] != -1) { // 범위 안에 있으면
-                    if (map[nr][nc] == find) return false;
-                    nr += dr[i];
-                    nc += dc[i];
-                }
-            }
-        }
-
-        return true;
     }
 
 }
---
+++ d0930/bj16928.java
@@ -0,0 +1,103 @@
+package d0930;
+
+import java.io.*;
+import java.util.*;
+
+
+public class bj16928 { // 80ms
+    public static void main(String[] args) throws Exception {
+        // 참고 솔루션
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 빠른 읽기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashMap<Integer, Integer> slide = new HashMap<>();
+
+        for (int i=0; i<n+m; i++) {
+            st = new StringTokenizer(br.readLine());
+            slide.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        boolean[] visited = new boolean[101];
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {1, 0});
+
+        while(!q.isEmpty()) {
+            int cur = q.peek()[0];
+            int cnt  = q.peek()[1];
+            q.poll();
+            for (int i=1; i<=6; i++) {
+                int nx = cur + i;
+                if (nx == 100) {
+                    System.out.println(cnt+1);
+                    return;
+                } else if (nx < 100) {
+                    while (slide.containsKey(nx)) {
+                        nx = slide.get(nx);
+                    }
+                    if (!visited[nx]) { // 안가봤으면
+                        visited[nx] = true;
+                        q.offer(new int[] {nx, cnt+1});
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    // 내 솔루션 수정
+    public class Main{
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 빠른 읽기
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            HashMap<Integer, Integer> ladder = new HashMap<>();
+            HashMap<Integer, Integer> snake = new HashMap<>();
+
+            for (int i=0; i<n; i++) {
+                st = new StringTokenizer(br.readLine());
+                ladder.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            }
+
+            for (int i=0; i<m; i++) {
+                st = new StringTokenizer(br.readLine());
+                snake.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            }
+
+
+            int[] dp = new int[101];
+
+            Queue<Integer> q = new ArrayDeque<>();
+            q.offer(1);
+
+            while(!q.isEmpty()) {
+                int cur = q.poll();
+                for (int i=1; i<=6; i++) {
+                    int next = cur + i;
+                    if (next == 100) {
+                        System.out.println(dp[cur] + 1);
+                        return;
+                    }
+                    if (ladder.containsKey(next)) {
+                        next = ladder.get(next);
+                    } else if (snake.containsKey(next)) {
+                        next = snake.get(next);
+                    }
+                    if (dp[next] == 0) { // 안가본데 확인
+                        dp[next] = dp[cur] + 1;
+                        q.offer(next);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
