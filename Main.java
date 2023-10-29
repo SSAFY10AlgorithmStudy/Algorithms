88cd25b4d85bfe14b920f9dd482829e45e06ee1f
박종우 <jongwoop97@gmail.com>
2023-10-30 00:24:15 +0900

    1028

--- Main.java
+++ Main.java
@@ -3,37 +3,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 빠른 읽기
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int t = Integer.parseInt(st.nextToken());
-        for (int tc=0; tc<t; tc++) {
-            int n = Integer.parseInt(br.readLine());
-            if (n == 0) {
-                sb.append(0).append("\n");
-                continue;
-            }
+        int n = Integer.parseInt(st.nextToken());
 
-            Map<String, Integer> clothes = new HashMap<>();
-
-            for (int i=0; i<n; i++) {
-                st = new StringTokenizer(br.readLine());
-                st.nextToken();
-                String c = st.nextToken();
-                clothes.putIfAbsent(c, 0);
-                clothes.put(c, clothes.get(c)+1);
-            }
-
-            System.out.println(clothes);
-
-            int ans = 1;
-            for(int x: clothes.values()) {
-                ans *= x+1;
-            }
-            sb.append(ans-1).append("\n");
-        }
 
         bw.write(sb.toString());
         bw.flush();
--- d0930/bj16928.java
+++ d0930/bj16928.java
@@ -50,54 +50,54 @@ public class bj16928 { // 80ms
     }
 
     // 내 솔루션 수정
-    public class Main{
-        public static void main(String[] args) throws Exception {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 빠른 읽기
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-
-            HashMap<Integer, Integer> ladder = new HashMap<>();
-            HashMap<Integer, Integer> snake = new HashMap<>();
-
-            for (int i=0; i<n; i++) {
-                st = new StringTokenizer(br.readLine());
-                ladder.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            }
-
-            for (int i=0; i<m; i++) {
-                st = new StringTokenizer(br.readLine());
-                snake.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-            }
-
-
-            int[] dp = new int[101];
-
-            Queue<Integer> q = new ArrayDeque<>();
-            q.offer(1);
-
-            while(!q.isEmpty()) {
-                int cur = q.poll();
-                for (int i=1; i<=6; i++) {
-                    int next = cur + i;
-                    if (next == 100) {
-                        System.out.println(dp[cur] + 1);
-                        return;
-                    }
-                    if (ladder.containsKey(next)) {
-                        next = ladder.get(next);
-                    } else if (snake.containsKey(next)) {
-                        next = snake.get(next);
-                    }
-                    if (dp[next] == 0) { // 안가본데 확인
-                        dp[next] = dp[cur] + 1;
-                        q.offer(next);
-                    }
-                }
-            }
-        }
-
-    }
+//    public class Main{
+//        public static void main(String[] args) throws Exception {
+//            BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 빠른 읽기
+//            StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//            int n = Integer.parseInt(st.nextToken());
+//            int m = Integer.parseInt(st.nextToken());
+//
+//            HashMap<Integer, Integer> ladder = new HashMap<>();
+//            HashMap<Integer, Integer> snake = new HashMap<>();
+//
+//            for (int i=0; i<n; i++) {
+//                st = new StringTokenizer(br.readLine());
+//                ladder.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+//            }
+//
+//            for (int i=0; i<m; i++) {
+//                st = new StringTokenizer(br.readLine());
+//                snake.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+//            }
+//
+//
+//            int[] dp = new int[101];
+//
+//            Queue<Integer> q = new ArrayDeque<>();
+//            q.offer(1);
+//
+//            while(!q.isEmpty()) {
+//                int cur = q.poll();
+//                for (int i=1; i<=6; i++) {
+//                    int next = cur + i;
+//                    if (next == 100) {
+//                        System.out.println(dp[cur] + 1);
+//                        return;
+//                    }
+//                    if (ladder.containsKey(next)) {
+//                        next = ladder.get(next);
+//                    } else if (snake.containsKey(next)) {
+//                        next = snake.get(next);
+//                    }
+//                    if (dp[next] == 0) { // 안가본데 확인
+//                        dp[next] = dp[cur] + 1;
+//                        q.offer(next);
+//                    }
+//                }
+//            }
+//        }
+//
+//    }
 
 }
